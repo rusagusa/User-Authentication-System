5ec23154dc98e4df1fdefa151634abc8409869dc
rusagusa <rusagusa.jeix@gmail.com>
2025-09-19 21:57:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
-
-// Serve static frontend files
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.use(express.static(path.join(__dirname, "public")));
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -99,4 +100,5 @@ app.get("/dashboard", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
 
